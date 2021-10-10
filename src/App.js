17f80205c09bd5a14e6313ec7d0fe6e97b59a04c
Md Asad Azam <mdasadazamlucky@gmail.com>
2021-10-10 13:01:32 +0530

    Added sidebar #HardCoded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
       {/* Header */}
       <Header />
 
       {/* body */}
-        {/* sidebar */}
+        <div className="app_body">
+          <Sidebar />
+        </div>
         {/* feed */}
         {/* widgets */}
     </div>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import { Avatar } from '@mui/material';
+import React from 'react'
+import "./Sidebar.css";
+export default function Sidebar() {
+
+    const recentItems = (topic) => (
+        <div className="sidebar_recent">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar_top">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
+                <Avatar className="sidebar_avatar" />
+                <h2>Asad</h2>
+                <h4>asad@asad.gmail.com</h4>
+
+
+            </div>
+
+            <div className="sidebar_stats">
+                <div className="sidebar_stat">
+                    <p>Who viewed your profile</p>
+                    <p className="sidebar_statnum">2,013</p>
+                </div>
+                <div className="sidebar_stat">
+                    <p>Views on your post</p>
+                    <p className="sidebar_statnum">2,673</p>
+                </div>
+            </div>
+
+            <div className="sidebar_bottom">
+                {recentItems("react")}
+                {recentItems("prog")}
+                {recentItems("software")}
+                {recentItems("devOps")}
+            </div>
+        </div>
+    )
+}
